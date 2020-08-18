5f4ea3f40e08abcefe0940fdd154425380a1f53c
bhagwanbankar <bhagwan.bankar312@gmail.com>
2020-08-18 19:42:55 +0530

    SpringSecurityCognito module added

---
+++ AWSCognitoDemo/src/main/java/org/bhagwan/cognito/SimpleController.java
@@ -0,0 +1,14 @@
+package org.bhagwan.cognito;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SimpleController {
+
+    @GetMapping(path = "/api/hello")
+    public String getResp(){
+        return  "Hey authenticated request!";
+    }
+
+}
